12b5215c7b464223ab66f6e2f9eba695e61ae502
bmarano-ux <bmarano@gbox.adnu.edu.ph>
2026-02-18 23:57:08 +0800

    Delete college-life-webpage directory

--- college-life-webpage/script.js
+++
@@ -1,5 +0,0 @@
-function showMotiv() {
-    const tipElement = document.getElementById("tipDisplay");
-    tipElement.innerText = "💡 Tip: Pick good friends and groupmates";
-    tipElement.style.display = "block";
-}
